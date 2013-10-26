2ce930bff0b83aedaa7e91023e06b185d2041146
Darragh Maguire Kane <dmaguirekane@gmail.com>
2013-10-26 21:58:59 +0100

    Fix window sizing bug

--- src/fontviewer/FontViewer.java
+++ src/fontviewer/FontViewer.java
@@ -1,9 +1,11 @@
 package fontviewer;
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.GraphicsEnvironment;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JList;
 import javax.swing.JPanel;
@@ -22,14 +24,15 @@ import javax.swing.event.ListSelectionListener;
 
 public class FontViewer
 {
-	private int[] fontSizes = {36, 24, 21, 18, 16, 14, 12, 11, 10, 9, 8, 7, 6}; //in pixels
+	private int[] fontSizes = {36, 30, 24, 21, 18, 16, 14, 12, 11, 10, 9, 8, 7, 6}; //in pixels
 	private final int FONT_SIZE = 40;
 	
 	private JFrame frame;
+	private ImageIcon icon;
 	private JTextPane tpExampleText;
 	private JList<String> fontList;
 	private JPanel panelMain;
-	private JScrollPane scrollPane;
+	private JScrollPane scrollFontList, scrollTextPane;
 
 	public static void main(String[] args)
 	{
@@ -59,7 +62,8 @@ public class FontViewer
 	{		
 		fontList = new JList<String>();
 		tpExampleText = new JTextPane();
-		scrollPane = new JScrollPane();
+		scrollFontList = new JScrollPane();
+		scrollTextPane = new JScrollPane();
 		panelMain = new JPanel();	
 		frame = new JFrame();
 		
@@ -71,22 +75,30 @@ public class FontViewer
 		fontList.setFont(new Font("Trebuchet MS", Font.PLAIN, 14));
 		
 		tpExampleText.setContentType("text/html");
-		fontChangeOccurred();
+		tpExampleText.setEditable(false);
+		tpExampleText.setFocusable(false);
 		tpExampleText.setBorder(new EmptyBorder(0, 20, 10, 20));		
 		tpExampleText.setFont(new Font(fontList.getSelectedValue(), Font.PLAIN, FONT_SIZE));
+		fontChangeOccurred();
 		
-		scrollPane.setViewportView(fontList);
+		scrollFontList.setViewportView(fontList);
+		
+		scrollTextPane.setViewportView(tpExampleText);
 			
 		panelMain.setLayout(new BorderLayout());
-		panelMain.add(scrollPane, BorderLayout.WEST);
-		panelMain.add(tpExampleText, BorderLayout.CENTER);
+		panelMain.add(scrollFontList, BorderLayout.WEST);
+		panelMain.add(scrollTextPane, BorderLayout.CENTER);
 		panelMain.setBackground(Color.WHITE);
+
+		icon = new ImageIcon(this.getClass().getResource("/fontviewer/resources/font_viewer_icon.png"));
 
 		frame.add(panelMain);
 		frame.setTitle("Font Viewer");
+		frame.setIconImage(icon.getImage());
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setLocationRelativeTo(null);
 		frame.setExtendedState(frame.getExtendedState() | JFrame.MAXIMIZED_BOTH);
+		frame.setMinimumSize(new Dimension(800, 600));
+		frame.setLocationRelativeTo(null);
 	}
 	
 	private ListSelectionListener fontListSelectionListener = new ListSelectionListener() 
@@ -104,9 +116,10 @@ public class FontViewer
 		
 		for(int i = 0; i < fontSizes.length; i++)
 		{
-			exampleText += "<p style='font-family:" + fontList.getSelectedValue() + "; font-size:" + (fontSizes[i]/16.0f)*100 + "%'>The quick brown fox jumps over the lazy dog (" + fontSizes[i] + "px)</p>";
+			exampleText += "<p style='font-family:" + fontList.getSelectedValue() + "; font-size:" + (fontSizes[i]/16.0f)*100 + "%'>(" + fontSizes[i] + "px) The quick brown fox jumps over the lazy dog</p>";
 		}
 		
 		tpExampleText.setText(exampleText);
+		tpExampleText.setCaretPosition(0);
 	}
 }
